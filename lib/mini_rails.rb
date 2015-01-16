c02c409c403e952cc6ce473c773fce3135ae1010
Ahmad <wisamfaithful@gmail.com>
2015-01-16 15:16:00 -0500

    routing

--- lib/mini_rails.rb
+++ lib/mini_rails.rb
@@ -3,7 +3,15 @@ require "mini_rails/version"
 module MiniRails
   class Application
   	def call(env)
-  	  [ 200, {'Content-type' => 'text/html' }, [ 'Hello'] ]
+  		controller, action = get_controller_and_action(env)
+  		response = controller.new.send(action)
+  	  [ 200, {'Content-type' => 'text/html' }, [ response ] ]
+  	end
+
+  	def get_controller_and_action(env)
+  		_, controller_name, action = env['PATH_INFO'].split('/')
+  		controller_name = controller_name.capitalize + 'Controller'
+  		[ Object.const_get(controller_name), action] 
   	end
   end
 end
